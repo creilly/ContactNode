299aee51f3db16b0958be80429de74b85ea3c61c
Christopher Reilly <reilly.christopher@gmail.com>
2012-01-29 02:28:27 -0600

    fixed upload stuff

--- requestHandlers.js
+++ requestHandlers.js
@@ -28,12 +28,13 @@ function upload(response, request) {
   console.log("Request handler 'upload' was called.");
 
   var form = new formidable.IncomingForm();
-  form.uploadDir = './tmp/';
+  form.uploadDir = "./tmp/";
   console.log("about to parse");
   form.parse(request, function(error, fields, files) {
     console.log("parsing done");
-    fs.renameSync(files.upload.path, "./tmp/test.png"); 
-    console.log("shitstorm");   
+    console.log(files.upload.path);
+    fs.renameSync(files.upload.path, "./tmp/test.png");
+
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("received image:<br/>");
     response.write("<img src='/show' />");
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function start(route, handle) {
     route(handle, pathname, response, request);
   }
 
-  http.createServer(onRequest).listen(process.env.PORT);
+  http.createServer(onRequest).listen(process.env.PORT || 8888);
   console.log("Server has started.");
 }
 
