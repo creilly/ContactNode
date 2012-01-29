14e9597f86d2293554096123f1799385c7488738
Christopher Reilly <reilly.christopher@gmail.com>
2012-01-29 14:18:13 -0600

    simplified server

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handle) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-    route(handle, pathname, response, request);
-  }
+    function onRequest(request, response) {
+	var pathname = url.parse(request.url).pathname;
+	console.log("Request for " + pathname + " received.");
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<p>URL requested:</p>");
+	response.write("<p>" + pathname + "</p>");
+	response.end();
+    }
 
-  http.createServer(onRequest).listen(process.env.PORT || 8888);
-  console.log("Server has started.");
+    http.createServer(onRequest).listen(process.env.PORT || 8888);
+    console.log("Server has started.");
 }
 
 exports.start = start;
